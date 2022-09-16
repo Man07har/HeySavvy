3ad81dbd0675068ac5120c2da05cf24227390877
Man07har <thakurmanoharkumar7@gmail.com>
2022-09-17 00:38:45 +0530

    LETS LOOK INTO THE MATTER

---
+++ DecodeString.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int temp;
+    int length=s.size();
+    for(int i=0;i<length;i++){
+        if(s[i]>='A' && s[i]<='Z'){
+           temp=s[i]-'A';
+           if(temp>=10) {
+            
+           }
+        }
+        if(s[i]>='a' && s[i]<='z'){
+            
+        }
+    }
+
+}
