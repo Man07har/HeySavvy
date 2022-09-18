3b1242c40b101ab9438b658380049742f073357e
Man07har <thakurmanoharkumar7@gmail.com>
2022-09-19 00:31:23 +0530

    this the decode string

--- DecodeString.cpp
+++ DecodeString.cpp
@@ -1,20 +1,34 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    string s;
-    cin>>s;
-    int temp;
-    int length=s.size();
-    for(int i=0;i<length;i++){
-        if(s[i]>='A' && s[i]<='Z'){
-           temp=s[i]-'A';
-           if(temp>=10) {
-            
-           }
-        }
-        if(s[i]>='a' && s[i]<='z'){
-            
+
+int solve(){
+    int n;
+    cin>>n;
+    unordered_set<int> st;
+    char arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]=='0' && (i==n-1 || arr[i+1]!='0')){
+            st.insert(i-2);
         }
     }
-
+    string res="";
+    for(int i=0;i<n;i++){
+        if(st.count(i)==1){
+            int temp=10*(arr[i]-'0')+(arr[i+1]-'0');
+            res+='a'+(arr[i]-'0'-1);
+        }
+    }
+    cout<<res<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
 }
