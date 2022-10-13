12f2f425d632947d4e0288248704e8a89868fffc
Man07har <thakurmanoharkumar7@gmail.com>
2022-10-13 10:12:48 +0530

    #DAY16 OF CODE

--- deleteNodeInTheLinkedList.cpp
+++ deleteNodeInTheLinkedList.cpp
@@ -43,7 +43,6 @@ public:
     static void deleteNode(ListNode* node) {
         ListNode* next = node->next;
         *node = *next;
-        // Don't be that person that leaks memory.
         delete next;
     }
 };
