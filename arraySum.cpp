37117be0610c587d57cca8a33a00f7972a939c59
Man07har <thakurmanoharkumar7@gmail.com>
2022-10-15 02:41:17 +0530

    #DAY17 OF CODE

---
+++ arraySum.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    void solve(int a, int b, int c){
+     if(a==(b+c)){
+     cout<<"YES";
+ }
+ else if(b==(a+c))
+ {
+     cout<<"YES";
+ }
+ else if(c==(a+b)){
+     cout<<"YES";
+ }
+ else
+ cout<<"NO";
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+    t++;
+    while(t--){
+       int a[3];
+       for(int i=0;i<3;i++)
+       {
+           cin>>a[i];
+       }
+       solve(a[1],a[2],a[3]);
+    }
+    return 0;
+}
