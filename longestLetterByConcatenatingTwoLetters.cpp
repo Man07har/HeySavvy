ff4001fd99425d9b77ef85bb9bcc9209722de55e
Man07har <thakurmanoharkumar7@gmail.com>
2022-11-03 20:07:37 +0530

    #DAY35 OF CODE

---
+++ longestLetterByConcatenatingTwoLetters.cpp
@@ -0,0 +1,71 @@
+/* You are given an array of strings words. Each element of words consists of two lowercase English letters.
+
+Create the longest possible palindrome by selecting some elements from words and concatenating them in any order. Each element can be selected at most once.
+
+Return the length of the longest palindrome that you can create. If it is impossible to create any palindrome, return 0.
+
+A palindrome is a string that reads the same forward and backward.
+
+ 
+
+Example 1:
+
+Input: words = ["lc","cl","gg"]
+Output: 6
+Explanation: One longest palindrome is "lc" + "gg" + "cl" = "lcggcl", of length 6.
+Note that "clgglc" is another longest palindrome that can be created.
+Example 2:
+
+Input: words = ["ab","ty","yt","lc","cl","ab"]
+Output: 8
+Explanation: One longest palindrome is "ty" + "lc" + "cl" + "yt" = "tylcclyt", of length 8.
+Note that "lcyttycl" is another longest palindrome that can be created.
+Example 3:
+
+Input: words = ["cc","ll","xx"]
+Output: 2
+Explanation: One longest palindrome is "cc", of length 2.
+Note that "ll" is another longest palindrome that can be created, and so is "xx".
+ 
+
+Constraints:
+
+1 <= words.length <= 105
+words[i].length == 2
+words[i] consists of lowercase English letters. */
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        int count=0;
+     
+        unordered_map<string,int> um;
+        for(auto w:words) {
+            um[w]++;
+        }
+        
+     
+        bool flag=false;
+        for(auto x:words) {
+            string wrd=x;
+            reverse(wrd.begin(),wrd.end());
+          
+            if(wrd!=x && um[x]>0 && um[wrd]>0) {
+                um[x]--;
+                um[wrd]--;
+                count+=4;
+            }
+            else if(wrd==x && um[x]>1) {
+                um[x]-=2;
+                count+=4;
+            } 
+            else if(wrd==x && !flag && um[x]>0) {
+                um[x]--;
+                count+=2;
+                flag=true;
+            }
+        }
+        return count;
+        
+    }
+};
