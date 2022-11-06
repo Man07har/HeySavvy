0d61b4f84f4ecfab2f4dcbf9b9d9380e63b0259c
Man07har <thakurmanoharkumar7@gmail.com>
2022-11-06 10:03:46 +0530

    #DAY38 OF CODE

---
+++ minimumNumberOfCoins.cpp
@@ -0,0 +1,69 @@
+/* Given an infinite supply of each denomination of Indian currency { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 } and a target value N.
+Find the minimum number of coins and/or notes needed to make the change for Rs N. You must return the list containing the value of coins required. 
+
+
+Example 1:
+
+Input: N = 43
+Output: 20 20 2 1
+Explaination: 
+Minimum number of coins and notes needed 
+to make 43. 
+
+Example 2:
+
+Input: N = 1000
+Output: 500 500
+Explaination: minimum possible notes
+is 2 notes of 500.
+
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function minPartition() which takes the value N as input parameter and returns a list of integers in decreasing order.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+
+Constraints:
+1 ≤ N ≤ 106 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+   vector<int> minPartition(int N)
+    {
+        // code here
+        vector<int>ans;
+        vector<int>rupee={1,2,5,10,20,50,100,200,500,2000};
+        while(N>0){
+            for(int i=9;i>=0;i--){
+                if(N>=rupee[i]){
+                     N-=rupee[i];
+                    ans.push_back(rupee[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> numbers = ob.minPartition(N);
+        for(auto u: numbers)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
