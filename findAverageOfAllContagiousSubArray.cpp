dc26ef414ed2fe974b2341663a02ae44d26f0e85
Man07har <thakurmanoharkumar7@gmail.com>
2022-12-08 01:53:46 +0530

    #DAY70 OF CODE

---
+++ findAverageOfAllContagiousSubArray.cpp
@@ -0,0 +1,18 @@
+/* Given an array, find the average of all contiguous subarrays of size ‘K’ in it. */
+
+//brute force Approach
+
+class solution {
+    public:
+    int averageOfSubArray(int arr[]){
+        int k;
+        int sum=0;
+        for(int i=0;i<arr.size();i++)
+        { 
+            sum+=arr[i];
+            for(int j=i;j<i+1;j++){
+                
+            }
+        }
+    }
+}
