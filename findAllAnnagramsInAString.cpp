c76ae134aa427d7421cb113d1f1bc94c253a4ae7
Man07har <thakurmanoharkumar7@gmail.com>
2023-02-05 20:46:32 +0530

    #DAY119 OF CODE

---
+++ findAllAnnagramsInAString.cpp
@@ -0,0 +1,65 @@
+/* Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+
+Input: s = "abab", p = "ab"
+Output: [0,1,2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+ 
+
+Constraints:
+
+1 <= s.length, p.length <= 3 * 104
+s and p consist of lowercase English letters. */
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        vector<int> hash(26, 0);
+        vector<int> phash(26, 0);
+        int window = p.size();
+        int len = s.size();
+        if(len < window)
+        {
+            return ans;
+        }
+        int left = 0,right = 0;
+        while(right < window)
+        {
+            phash[p[right] - 'a'] += 1;
+            hash[s[right] - 'a'] += 1;
+            right++;
+        }
+        right -=1;
+        while(right < len)
+        {
+            if(phash == hash)
+            {
+                ans.push_back(left);
+            }
+            right+=1;
+            if(right != len)
+            {
+                hash[s[right] - 'a'] += 1;
+            }
+            hash[s[left] - 'a'] -=1 ;
+            left += 1;
+        }
+        return ans;
+    }
+};
