ff610078afbca765a106b6a4dab118376596816a
Man07har <thakurmanoharkumar7@gmail.com>
2023-02-15 21:18:27 +0530

    #DAY129 OF CODE

--- team.cpp
+++ team.cpp
@@ -28,18 +28,4 @@ In the first sample Petya and Vasya are sure that they know how to solve the fir
 In the second sample the friends will only implement the second problem, as Vasya and Tonya are sure about the solution.
 
  */
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int a,b,c;
-        cin>>a>>b>>c;
-        if(a+b+c>=2){
-            cout<<"1"<<endl;
-        }
-        else
-        cout<<"0"<<endl;
-    }
-}
+
