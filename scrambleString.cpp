6d97d68b7f1c3a48b6d83f5606d337af328d9bda
Man07har <thakurmanoharkumar7@gmail.com>
2023-03-30 19:40:02 +0530

    #DAY162 OF CODE

---
+++ scrambleString.cpp
@@ -0,0 +1,77 @@
+/* We can scramble a string s to get a string t using the following algorithm:
+
+If the length of the string is 1, stop.
+If the length of the string is > 1, do the following:
+Split the string into two non-empty substrings at a random index, i.e., if the string is s, divide it to x and y where s = x + y.
+Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
+Apply step 1 recursively on each of the two substrings x and y.
+Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s1 = "great", s2 = "rgeat"
+Output: true
+Explanation: One possible scenario applied on s1 is:
+"great" --> "gr/eat" // divide at random index.
+"gr/eat" --> "gr/eat" // random decision is not to swap the two substrings and keep them in order.
+"gr/eat" --> "g/r / e/at" // apply the same algorithm recursively on both substrings. divide at random index each of them.
+"g/r / e/at" --> "r/g / e/at" // random decision was to swap the first substring and to keep the second substring in the same order.
+"r/g / e/at" --> "r/g / e/ a/t" // again apply the algorithm recursively, divide "at" to "a/t".
+"r/g / e/ a/t" --> "r/g / e/ a/t" // random decision is to keep both substrings in the same order.
+The algorithm stops now, and the result string is "rgeat" which is s2.
+As one possible scenario led s1 to be scrambled to s2, we return true.
+Example 2:
+
+Input: s1 = "abcde", s2 = "caebd"
+Output: false
+Example 3:
+
+Input: s1 = "a", s2 = "a"
+Output: true
+ 
+
+Constraints:
+
+s1.length == s2.length
+1 <= s1.length <= 30
+s1 and s2 consist of lowercase English letters. */
+
+
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int n = (int) s1.size();
+        bool vis[n][n][n][n];
+        bool dp[n][n][n][n];
+        memset(vis, false, sizeof(vis));
+        memset(dp, false, sizeof(dp));
+        function<bool(int, int, int, int)> helper = [&] (int l1, int r1, int l2, int r2) {
+            if (vis[l1][r1][l2][r2]) return dp[l1][r1][l2][r2];
+            vis[l1][r1][l2][r2] = true;
+
+            bool isSame = true;
+            for (int i = l1, j = l2; i <= r1 && j <= r2; i++, j++)
+                if (s1[i] != s2[j]) isSame = false;
+            
+            if (isSame) {
+                dp[l1][r1][l2][r2] = true;
+                return true;
+            }
+
+            bool res = false;
+            for (int i = l1; i < r1; i++) {
+               
+                res |= (helper(l1, i, l2, l2 + (i - l1)) && helper(i + 1, r1, l2 + (i - l1) + 1, r2));
+
+                res |= (helper(l1, i, r2 - (i - l1), r2) && helper(i + 1, r1, l2, r2 - (i - l1) - 1));
+            }
+
+            dp[l1][r1][l2][r2] = res;
+            return res;
+        };
+
+        return helper(0, n - 1, 0, n - 1);
+    }
+};
