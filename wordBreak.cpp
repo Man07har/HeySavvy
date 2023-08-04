2e7dd34791d5cea572d1336966ec8646644ac1cf
Man07har <thakurmanoharkumar7@gmail.com>
2023-08-04 18:17:07 +0530

    #DAY227 OF CODE

---
+++ wordBreak.cpp
@@ -0,0 +1,55 @@
+/* Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique. */
+
+class Solution {
+public:
+    map<string,int>m;
+    bool help(int i,string &s,vector<int>&dp){
+        if(i==s.size())return true;
+        if(dp[i]!=-1)return dp[i];
+        bool ans = false;
+        string a = "";
+        for(int k=i;k<s.size();k++){
+            a += s[k];
+            if(m[a]){
+                ans |= help(k+1,s,dp);
+            }
+        }
+        return  dp[i] = ans;
+    }
+    bool wordBreak(string s, vector<string>& w) {
+        for(auto i:w){
+            m[i]++;
+        }
+        vector<int>dp(s.size(),-1);
+        return help(0,s,dp);
+    }
+};
