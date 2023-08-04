649e281fc301e77937736e9f031c28c5c39cb4c8
Man07har <thakurmanoharkumar7@gmail.com>
2023-08-04 18:18:44 +0530

    #DAY227 OF CODE

---
+++ reverseAStack.cpp
@@ -0,0 +1,51 @@
+/* You are given a stack St. You have to reverse the stack using recursion.
+
+Example 1:
+
+Input:
+St = {3,2,1,7,6}
+Output:
+{6,7,1,2,3}
+Explanation:
+Input stack after reversing will look like the stack in the output.
+Example 2:
+
+Input:
+St = {4,3,9,6}
+Output:
+{6,9,3,4}
+Explanation:
+Input stack after reversing will look like the stack in the output.
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function Reverse() which takes the stack St as input and reverses the given stack.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 <= size of the stack <= 104
+-109 <= Each element of the stack <= 109
+Sum of N over all test cases doesn't exceeds 106
+Array may contain duplicate elements.  */
+
+
+class Solution{
+public:
+    
+    void Reverse(stack<int> &St){
+        int arr[St.size()];
+        int i=0;
+        while(St.size()!=0){
+            arr[i]=St.top();
+            St.pop();
+            i++;
+        }
+        for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+            St.push(arr[i]);
+        }
+        
+        
+        
+    }
+};
